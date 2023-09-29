415e51ce9af81e31f5914bbf82422c21ccaf1ba2
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-09-29 18:48:58 -0400

    feat: Add firebase integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,17 @@ export default defineNuxtConfig({
   },
   modules: [
     "@nuxtjs/tailwindcss",
+    "nuxt-vuefire"
   ],
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyDPKEOY99oWSdi7zj08aLsoWUKl_H3Efb4",
+      authDomain: "max-and-maxer.firebaseapp.com",
+      projectId: "max-and-maxer",
+      appId: "1:951535404287:web:5bb70303b68dcfb02438ca",
+    },
+    auth: {
+      enabled: true,
+    },
+  },
 })
