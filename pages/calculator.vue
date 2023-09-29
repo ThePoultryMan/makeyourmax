57f8009166ad3417d60e7ddb30cbfbcde13f5f76
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-09-29 19:52:24 -0400

    feat: Adjust margins

--- pages/calculator.vue
+++ pages/calculator.vue
@@ -2,14 +2,14 @@
   <div class="flex flex-col items-center my-11">
     <form @submit="calculate" @submit.prevent="onSubmit">
       <NumberInput :set-value="setWeight" class="m-1 bg-accent-500 p-1.5 rounded-lg" />
-      <div class="flex justify-center my-1">
+      <div class="flex justify-center my-3">
         <label for="round" class="inline-block h-9 p-1 border border-accent-500 rounded-l-lg">Round To </label>
         <select v-bind="round" name="round" class="h-9 p-1 bg-accent-500 rounded-r-lg">
           <option :value="5">Five</option>
         </select>
       </div>
     </form>
-    <div v-show="calculated" class="w-2/3">
+    <div v-show="calculated" class="w-2/3 m-1">
       <Percentages :ready="calculated" :round="round" :weight="(weightInput)" />
     </div>
   </div>
