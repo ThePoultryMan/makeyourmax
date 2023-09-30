b0220b0573423fc3d60fd653d1db07d0350d521c
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-09-29 20:54:49 -0400

    refactor: Remove "calculator" page

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="p-3 flex justify-between bg-background-950">
     <div class="w-full flex gap-3 justify-between md:justify-normal md:w-auto">
       <span><NuxtLink to="/">Max & Maxer</NuxtLink></span>
-      <NuxtLink to="/calculator">Calculator</NuxtLink>
+      <span><NuxtLink to="/login">Login</NuxtLink></span>
     </div>
     <div class="hidden md:flex gap-3">
       <NuxtLink to="/login">Login</NuxtLink>
