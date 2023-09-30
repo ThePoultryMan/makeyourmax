4dc9e2afa8c3e64e861d9d74e62e330171c07d11
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-09-30 17:42:26 -0400

    chore: Remove "login" and "sign-up" pages.

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,12 +1,7 @@
 <template>
   <nav class="p-3 flex justify-between bg-background-950">
-    <div class="w-full flex gap-3 justify-between md:justify-normal md:w-auto">
+    <div class="w-full flex gap-3 justify-between">
       <span><NuxtLink to="/">Max & Maxer</NuxtLink></span>
-      <span><NuxtLink to="/login">Login</NuxtLink></span>
-    </div>
-    <div class="hidden md:flex gap-3">
-      <NuxtLink to="/login">Login</NuxtLink>
-      <NuxtLink to="/sign-up">Sign-Up</NuxtLink>
     </div>
   </nav>
 </template>
