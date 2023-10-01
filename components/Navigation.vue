10f05a8acb6b6bd4e2621ad39e0ebe37cf77f6af
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-01 12:18:52 -0400

    chore: Remove unused/non-functioning button

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,6 @@
   <nav class="p-3 flex justify-between bg-background-950">
     <div class="w-full flex gap-3 justify-between">
       <span><NuxtLink to="/">Max & Maxer</NuxtLink></span>
-      <button @click="$pwa.install()">&plus;</button>
     </div>
   </nav>
 </template>
