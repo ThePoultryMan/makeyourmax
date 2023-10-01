b30276fef0a9f09aae8fb04784cfd6de291176c5
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-09-30 21:45:24 -0400

    feat: Add logos and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,26 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
+      link: [
+        // General implementation from https://css-tricks.com/svg-favicons-and-all-the-fun-things-we-can-do-with-them/
+        {
+          rel: "icon",
+          href: "/favicon.ico",
+          sizes: "any",
+        },
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "/favicon.svg",
+        },
+      ],
       title: "Max & Maxer",
     },
   },
   modules: [
     "@nuxtjs/tailwindcss",
     "nuxt-vuefire",
-    "@vite-pwa/nuxt",
+    "@vite-pwa/nuxt"
   ],
   vuefire: {
     config: {
@@ -23,7 +36,26 @@ export default defineNuxtConfig({
     manifest: {
       name: "Max & Maxer",
       short_name: "Maxer",
-      theme_color: "#161221",
+      theme_color: "#0d0b14",
+      background_color: "161221",
+      icons: [
+        {
+          src: "maxer-192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "maxer-512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "maxer-512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
     },
   },
 });
