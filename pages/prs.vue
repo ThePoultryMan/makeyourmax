b94d0fcea4b51621853e5ec13a7573172a8efa81
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-01 22:57:53 -0400

    chore: Fix empty content on "prs"

--- pages/prs.vue
+++ pages/prs.vue
@@ -1,1 +1,6 @@
-<template></template>
+<template>
+  <header class="flex flex-col items-center my-16">
+    <h1 class="text-lg">not yet available...</h1>
+    <p><i>but</i>, I appreciate you being here. This is the next (major) feature being made.</p>
+  </header>
+</template>
