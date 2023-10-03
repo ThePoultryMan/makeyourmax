633b68a52bd5d04e096274178bd703c40cd9101b
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-02 21:10:58 -0400

    fix: <Sidebar> no longer making page scroll

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="absolute top-0 right-0 bg-accent-800 overflow-x-hidden">
+  <div class="absolute top-0 right-0 bg-accent-800 overflow-hidden">
     <Transition name="sidebar">
       <div v-show="props.activated" id="sidebar" class="h-screen">
         <button @click="emits('hide-sidebar')" class="text-lg absolute top-0 right-0 m-3">&times;</button>
-        <nav class="ml-12 mt-24 flex flex-col text-2xl gap-3">
+        <nav class="pl-12 pt-24 flex flex-col text-2xl gap-3">
           <NuxtLink to="/">Home</NuxtLink>
           <NuxtLink to="/prs">PRs</NuxtLink>
         </nav>
