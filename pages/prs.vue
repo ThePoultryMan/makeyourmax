c717a71304b515fc803eb2184674f299b930bf9b
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-04 22:59:58 -0400

    feat: Direct prs to roadmap

--- pages/prs.vue
+++ pages/prs.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex flex-col items-center my-16">
     <h1 class="text-lg">not yet available...</h1>
-    <p><i>but</i>, I appreciate you being here. This is the next (major) feature being made.</p>
+    <p>Check out the <NuxtLink to="/roadmap" class="text-purple-400">Roadmap</NuxtLink> for more information.</p>
   </header>
 </template>
