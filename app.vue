fd467388b3a896c13bc6a6c55a17280c3be89c4e
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-05 22:24:53 -0400

    feat: Loading symbol for FOUC

--- app.vue
+++ app.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="text-text-400 max-h-screen overflow-hidden overscroll-none">
-    <VitePwaManifest />
-    <div class="flex flex-col relative z-0">
-      <Navigation @open-sidebar="showSidebar(true)" />
-      <NuxtPage class="relative z-0" />
+    <div v-if="ready">
+      <VitePwaManifest />
+      <div class="flex flex-col relative z-0">
+        <Navigation @open-sidebar="showSidebar(true)" />
+        <NuxtPage class="relative z-0" />
+      </div>
+      <Sidebar @hide-sidebar="showSidebar(false)" :activated="sidebar" class="absolute top-0 right-0 bg-accent-800 overflow-x-hidden h-screen" />
     </div>
-    <Sidebar @hide-sidebar="showSidebar(false)" :activated="sidebar" class="absolute top-0 right-0 bg-accent-800 overflow-x-hidden h-screen" />
+    <div v-else class="bg-[#161221] min-h-screen flex justify-around items-center">
+      <Icon id="loadSymbol" name="mingcute:loading-3-line" size="9rem" class="text-gray-200" />
+    </div>
   </div>
 </template>
 
@@ -15,6 +20,7 @@ import themes from "~/assets/themes.json";
 
 const { $setupSave, $getSaveData } = useNuxtApp();
 
+const ready = ref(false);
 const sidebar = ref(false);
 
 let root: any;
@@ -37,6 +43,7 @@ onBeforeMount(async () => {
   if (root) {
     setThemeVariables(themes[themeName]);
   }
+  ready.value = true;
 });
 
 function showSidebar(show: boolean) {
@@ -51,3 +58,18 @@ function setThemeVariables(theme: any) {
   }
 }
 </script>
+
+<style>
+#loadSymbol {
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
