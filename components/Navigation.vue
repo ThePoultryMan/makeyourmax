5e9ca108da9da2afcbe5f4595c6bfdc1db9ad405
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-07 21:34:06 -0400

    style: Navigation changes

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-3 flex justify-between bg-background-950">
-    <div class="w-full flex gap-3 justify-between text-lg">
+  <div class="p-3 bg-background-950">
+    <div class="w-full flex items-center gap-3 justify-between text-lg">
       <span class="font-semibold"><NuxtLink to="/">Max & Maxer</NuxtLink></span>
       <button @click="emits('openSidebar')" class="text-2xl">&plus;</button>
     </div>
