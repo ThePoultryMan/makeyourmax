ed1cbfde172040c4cd448799cc90fc7255f58923
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-08 02:05:25 -0400

    feat: Initial pr system

--- pages/prs.vue
+++ pages/prs.vue
@@ -1,6 +1,37 @@
 <template>
-  <header class="flex flex-col items-center my-16">
-    <h1 class="text-lg">not yet available...</h1>
-    <p>Check out the <NuxtLink to="/roadmap" class="text-purple-400">Roadmap</NuxtLink> for more information.</p>
-  </header>
+  <div>
+    <header class="flex flex-col items-center my-16">
+      <h1 class="text-lg">Personal Records</h1>
+    </header>
+    <div class="grid">
+      <NuxtLink v-for="value, key in prs" :to="'/m/' + key">
+        <div>
+          <h2>{{ toTitle(key) }}</h2>
+          <p>{{ value }}</p>
+        </div>
+      </NuxtLink>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import movements from "~/assets/movements.json";
+
+const { $getSaveData, $setSaveData } = useNuxtApp();
+
+const prs = ref(await $getSaveData("prs"));
+
+onMounted(() => {
+  if (!prs.value) { prs.value = {}; }
+  for (const movement of movements) {
+    if (!(movement in prs.value)) {
+      prs.value[movement] = null;
+    }
+  }
+  $setSaveData("prs", toRaw(prs.value));
+});
+
+function toTitle(s: string) {
+  return s.replace("/([A-Z])/g", " $1").replace("/^./", (str) => str.toUpperCase());
+}
+</script>
