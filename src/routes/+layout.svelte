ce514353624951fc24314d9e788f266d66df948e
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-14 15:04:43 -0400

    rewrite: Add vite-pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script>
 	import '../app.postcss';
+	import { pwaInfo } from "virtual:pwa-info";
 
 	import Navigation from "$components/Navigation.svelte";
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : "";
 </script>
+
+<svelte:head>
+	{@html webManifestLink}
+</svelte:head>
 
 <Navigation />
 <slot />
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: {
+				name: "Max & Maxer",
+				short_name: "Maxer",
+				description: "An app to keep track of your PRs.",
+				lang: "en",
+				dir: "ltr",
+				start_url: "/",
+				display: "standalone",
+				display_override: [ "windows-control-overlay" ],
+				orientation: "portrait",
+				id: "/",
+			},
+		}),
+	],
 });
