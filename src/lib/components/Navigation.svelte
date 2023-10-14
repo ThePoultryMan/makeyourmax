fb82b1fc8ce6ae4e1c842308381d7b8b10ffd1e3
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-14 02:43:59 -0400

    rewrite: Component alias + initial header component

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,6 @@
+<div class="p-3 bg-background-950">
+  <div class="w-full flex items-center gap-3 justify-between text-lg">
+    <span class="font-semibold">Max & Maxer</span>
+    <button class="text-2xl">&plus;</button>
+  </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.postcss';
+
+	import Navigation from "$components/Navigation.svelte";
 </script>
 
+<Navigation />
 <slot />
