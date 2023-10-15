d48e3fb258534ff2d7c94eaa4542e9fbf4252d0a
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-15 16:32:38 -0400

    rewrite: Fix up some styles

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -3,7 +3,7 @@
   const dispatch = createEventDispatcher();
 </script>
 
-<div class="p-3 bg-background-950">
+<div class="p-3 bg-background-950 text-text-400">
   <div class="w-full flex items-center gap-3 justify-between text-lg">
     <span class="font-semibold">Max & Maxer</span>
     <button on:click={() => dispatch("sidebar-open", true)} class="text-2xl">&plus;</button>
