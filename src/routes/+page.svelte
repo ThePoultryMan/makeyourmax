d1bc2261e2c05eb86e5ca5a2434e7df00b1d46c1
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-15 19:30:29 -0400

    rewrite: Align elements center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
-    import LabeledInput from "$components/LabeledInput.svelte";
-    import PercentageTable from "$components/PercentageTable.svelte";
+  import LabeledInput from "$components/LabeledInput.svelte";
+  import PercentageTable from "$components/PercentageTable.svelte";
 
-    let weight = 0;
+  let weight = 0;
 </script>
 
-<LabeledInput inputId="weight-calculator" label="Weight: ">
+<div class="flex flex-col items-center [&>*]:my-3">
+  <LabeledInput inputId="weight-calculator" label="Weight: ">
     <input id="weight-calculator" type="number" bind:value={weight} />
-</LabeledInput>
-<PercentageTable weight={weight} />
+  </LabeledInput>
+  <PercentageTable {weight} />
+</div>
