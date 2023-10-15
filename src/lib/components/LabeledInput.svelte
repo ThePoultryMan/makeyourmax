938d99acaac92f5adf61e52191cc288b39a98002
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-15 18:49:56 -0400

    rewrite: Fix text color

--- src/lib/components/LabeledInput.svelte
+++ src/lib/components/LabeledInput.svelte
@@ -3,8 +3,8 @@
   export let label = "";
 </script>
 
-<div class="flex items-center text-lg">
-  <label for={inputId} class="inline-block text-text-400 h-9 p-1 border rounded-l-lg border-accent-500">{label}</label>
+<div class="flex items-center text-text-400 text-lg">
+  <label for={inputId} class="inline-block h-9 p-1 border rounded-l-lg border-accent-500">{label}</label>
   <div class="[&>*]:p-1 [&>*]:bg-background-400 [&>*]:rounded-r-lg">
     <slot />
   </div>
