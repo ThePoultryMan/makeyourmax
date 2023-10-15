3b567bb677f68b5ee3b0ed013cdff425ee097770
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-15 17:53:43 -0400

    rewrite: Initial LabeledInput

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+    import LabeledInput from "$components/LabeledInput.svelte";
     import PercentageTable from "$components/PercentageTable.svelte";
 </script>
 
+<LabeledInput inputId="weight-calculator" label="Weight: ">
+    <input id="weight-calculator" type="number" />
+</LabeledInput>
 <PercentageTable weight={0} />
