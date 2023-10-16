b3ebb1c82cef5f88e1d9879f5e7bd271900dabd7
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-15 20:02:35 -0400

    rewrite: Load & Save weight calculator value

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,22 @@
 <script>
+  import { onMount } from "svelte";
+
+  import { preferences } from "$lib/indy";
+
   import LabeledInput from "$components/LabeledInput.svelte";
   import PercentageTable from "$components/PercentageTable.svelte";
+    import { browser } from "$app/environment";
 
   let weight = 0;
+  $: {
+    if (browser) {
+      preferences.setItem("weightCalculator", weight);
+    }
+  }
+
+  onMount(async () => {
+    preferences.getItem("weightCalculator").then((value) => weight = value);
+  });
 </script>
 
 <div class="flex flex-col items-center [&>*]:my-3">
