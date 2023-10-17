aa3fe1ff259c143273223bcbec410003fa1c4eb6
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-16 22:18:57 -0400

    rewrite: Movement page

---
+++ src/routes/m/[movement]/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { page } from "$app/stores";
+  import { browser } from "$app/environment";
+
+  import { prs } from "$lib/indy";
+
+  import PercentageTable from "$components/PercentageTable.svelte";
+  import LabeledInput from "$components/LabeledInput.svelte";
+
+  let weight = 0;
+  $: {
+    if (browser) {
+      prData[$page.params.movement] = weight;
+      prs.setItem("prs", prData);
+    }
+  }
+  let prData: any = {};
+
+  onMount(async () => {
+    prs.getItem("prs").then((value: any) => {
+      if (value && value[$page.params.movement]) {
+        weight = value[$page.params.movement];
+        prData = value;
+      }
+    });
+  });
+
+  function toTitleCase(text: string) {
+    return text.replace(/([A-Z])/g, " $1").replace(/^./g, (str) => str.toUpperCase());
+  }
+</script>
+
+<div class="flex flex-col text-text-400 items-center">
+  <h1 class="my-8 text-xl font-semibold">{toTitleCase($page.params.movement)}</h1>
+  <div class="my-3">
+    <LabeledInput inputId="max" label="Max: ">
+      <input type="number" bind:value={weight} />
+    </LabeledInput>
+  </div>
+  <PercentageTable weight={typeof weight !== "string" ? weight : 0} />
+</div>
--- src/routes/prs/+page.svelte
+++ src/routes/prs/+page.svelte
@@ -32,7 +32,7 @@
   <title>PRs</title>
 </svelte:head>
 
-<div class="flex flex-wrap justify-center gap-3">
+<div class="flex flex-wrap justify-center gap-3 mx-5 text-text-400">
   {#each Object.entries(allPRs) as [movement, max]}
     <a href={"/m/" + movement} class="min-w-[264px] p-2 border border-primary-500 rounded-lg">
       <h2>{toTitleCase(movement)}</h2>
