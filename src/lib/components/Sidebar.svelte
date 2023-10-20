c6d1e640f5a70926bcc86e19e1f31415bee5f49a
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-10-20 01:17:19 -0400

    rewrite: Initial theme switching

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
 
+  import themeData from "$lib/assets/themes.json";
+
   export let open = false;
+  export let currentTheme = "";
 
   const dispatch = createEventDispatcher();
+
+  const themes = Object.keys(themeData);
+
+  function openDropdown() {}
+
+  function setTheme(theme: string) {
+    currentTheme = theme;
+    dispatch("set-theme", currentTheme);
+  }
 </script>
 
-<div class="fixed top-0 right-0 h-screen bg-accent-800 text-text-400 overflow-x-hidden sidebar" class:open-sidebar={open} class:close-sidebar={!open} >
+<div
+  class="fixed top-0 right-0 h-screen bg-accent-800 text-text-400 overflow-x-hidden sidebar"
+  class:open-sidebar={open}
+  class:close-sidebar={!open}
+>
   <div>
-    <button on:click={() => dispatch("sidebar-close", false)} class="absolute top-0 right-0 m-3 text-2xl">&times;</button>
+    <button
+      on:click={() => dispatch("sidebar-close", false)}
+      class="absolute top-0 right-0 m-3 text-2xl">&times;</button
+    >
     <nav class="px-12 py-24 text-2xl">
       <div class="flex flex-col gap-3 mb-3">
         <a href="/">Home</a>
@@ -19,6 +38,15 @@
         <a href="/roadmap">Roadmap</a>
       </div>
     </nav>
+    <button on:click={() => openDropdown()} class="mx-12 p-2 bg-accent-600 rounded-lg text-left">
+      <div>{themeData[currentTheme ? currentTheme : "myProd"].meta.name}</div>
+      <div class:min-h-0={true}>
+        {#each themes.filter((str) => str !== currentTheme) as theme}
+          <button on:click={() => setTheme(theme)}>{themeData[theme].meta.name}</button>
+        {/each}
+      </div>
+      <div />
+    </button>
   </div>
 </div>
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
   import "../app.postcss";
-  import { onMount } from "svelte";
+  import { browser } from "$app/environment";
   import { pwaInfo } from "virtual:pwa-info";
+  import { onMount } from "svelte";
 
-	import themes from "$lib/assets/themes.json";
+  import themes from "$lib/assets/themes.json";
   import { preferences } from "$lib/indy";
 
   import Navigation from "$components/Navigation.svelte";
@@ -14,8 +15,23 @@
   let sidebarOpen = false;
   let theme = "";
 
+  $: {
+    if (browser) {
+      preferences.setItem("theme", theme);
+      let themeData = themes[theme ? theme : "myProd"];
+      const root = document.querySelector(":root");
+      for (const [key, value] of Object.entries(themeData)) {
+        if (key !== "meta") {
+          for (const [selector, color] of Object.entries(value)) {
+            root.style.setProperty(`--${key}-${selector}`, color);
+          }
+        }
+      }
+    }
+  }
+
   onMount(async () => {
-    await preferences.getItem("theme", (themeValue) => {
+    await preferences.getItem("theme").then((themeValue) => {
       if (!themeValue) {
         preferences.setItem("theme", "myProd");
         theme = "myProd";
@@ -23,17 +39,6 @@
         theme = themeValue;
       }
     });
-
-		// Set Theme
-		let themeData = themes[theme];
-		const root = document.querySelector(":root");
-		for (const [key, value] of Object.entries(themeData)) {
-			if (key !== "meta") {
-				for (const [selector, color] of Object.entries(value)) {
-					root.style.setProperty(`--${key}-${selector}`, color);
-				}
-			}
-		}
   });
 </script>
 
@@ -43,4 +48,9 @@
 
 <Navigation on:sidebar-open={(value) => (sidebarOpen = value.detail)} />
 <slot />
-<Sidebar on:sidebar-close={(value) => (sidebarOpen = value.detail)} open={sidebarOpen} />
+<Sidebar
+  on:sidebar-close={(value) => (sidebarOpen = value.detail)}
+  on:set-theme={(value) => (theme = value.detail)}
+  open={sidebarOpen}
+  currentTheme={theme}
+/>
