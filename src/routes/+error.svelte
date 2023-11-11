b21945aefa54ddfda539e04da6a57d8bf7c25322
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-11-10 21:25:11 -0500

    feat: Create custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  function stripUrl(url: string) {
+    return url.replace(/([^.]*\/\/)/, "").replace("localhost:5173", "");
+  }
+</script>
+
+{#if $page.status === 404}
+  <div class="w-full text-center p-7">
+    <h1 class="mb-5 text-3xl">(404) Page Not Found</h1>
+    <p>
+      The page
+      <span class="p-1 bg-background-700 rounded-md">{stripUrl($page.url.toString())}</span>
+      could not be found. Double check the URL.
+    </p>
+    <br class="my-2" />
+    <a href="/" class="p-1.5 bg-background-950 rounded-lg">Go Home</a>
+  </div>
+{:else}
+  <div class="w-full text-center p-7">
+    <h1 class="mb-5 text-3xl">{$page.status}</h1>
+    <p>{$page.error?.message}</p>
+    <br class="my-2" />
+    <a href="/" class="p-1.5 bg-background-950 rounded-lg">Go Home</a>
+  </div>
+{/if}
