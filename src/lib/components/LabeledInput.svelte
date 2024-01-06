92d95d444f93f644ceadd64246e9a9aa8f457278
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-01-05 23:36:09 -0500

    style fix

--- src/lib/components/LabeledInput.svelte
+++ src/lib/components/LabeledInput.svelte
@@ -9,7 +9,7 @@
 <div class="flex items-center text-text-400 text-lg" class:flex-row-reverse={flipped}>
   <label
     for={inputId}
-    class="inline-block h-9 p-1 pr-2 border border-accent-500"
+    class="inline-block w-full h-9 p-1 pr-2 border border-accent-500"
     class:rounded-l-lg={!flipped}
     class:rounded-r-lg={flipped}>{label}</label
   >
