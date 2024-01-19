9468e9144b5eed78dde8a97e88160729bd5ae0fb
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-01-18 22:24:17 -0500

    feat: Plate math

--- src/lib/components/PercentageTable.svelte
+++ src/lib/components/PercentageTable.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
-  
+
+  import { weights } from "$lib/assets/weights.json";
+
   import LabeledInput from "$components/LabeledInput.svelte";
-  
+
   export let weight = 0;
   let round = 5;
 
@@ -23,6 +25,21 @@
       explain?.classList.add("hidden");
     }
   }
+
+  function calculateWeights(weight: number) {
+    let weightM = (weight - 45) / 2;
+    let barbellWeights = [];
+    while (weightM > 0) {
+      for (const weightI of weights) {
+        if (weightI <= weightM) {
+          barbellWeights.push(weightI);
+          weightM -= weightI;
+          break;
+        }
+      }
+    }
+    return barbellWeights;
+  }
 </script>
 
 <div class="w-full flex flex-col items-center">
@@ -39,7 +56,7 @@
   <div
     class="w-5/6 md:w-2/3 max-h-[60vh] text-2xl md:text-lg text-text-400 border border-accent-100 rounded-lg overflow-y-scroll"
   >
-    <table class="w-full border-separate border-spacing-0">
+    <table class="w-full border-separate border-spacing-0 table-fixed">
       <thead class="sticky top-0">
         <tr>
           <th class="bg-accent-600 border-r border-b">Percentage</th>
@@ -51,12 +68,26 @@
           <tr class="[&>td]:last:border-b-0 odd:bg-background-900 bg-background-950">
             <td class="p-2 md:p-1 border-r border-b">{(weightPercentages.length - i - 1) * 5}%</td>
             <td class="p-2 md:p-1 border-b">
-              <div class="flex justify-between items-center ">
+              <div class="flex justify-between items-center">
                 <span>{value}</span>
-                <button on:click={() => toggleExplain(((weightPercentages.length - i - 1) * 5).toString())}><Icon icon="ion:barbell" class="mr-3" /></button>
+                <button
+                  on:click={() =>
+                    toggleExplain(((weightPercentages.length - i - 1) * 5).toString())}
+                  ><Icon icon="ion:barbell" class="mr-3 text-xl" /></button
+                >
               </div>
-              <div id={(weightPercentages.length - i - 1) * 5 + "Explain"} class="hidden">
-                <p>g</p>
+              <div
+                id={(weightPercentages.length - i - 1) * 5 + "Explain"}
+                class="hidden ml-3 w-fit"
+              >
+                <span class="italic">Plates Required for a 45lbs. Bar:</span>
+                <ul>
+                  {#each calculateWeights(value) as weight}
+                    <li class="ml-1.5">- {weight}lbs.</li>
+                  {:else}
+                    <li class="ml-1.5">- Just The Bar</li>
+                  {/each}
+                </ul>
               </div>
             </td>
           </tr>
