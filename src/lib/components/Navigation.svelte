57b0e6d3ef7e584d1f63933763e5af244801f9bc
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-01-19 22:24:38 -0500

    refactor: Finish nav

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
-  import Icon from "@iconify/svelte";
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
+
+  import Icon from "@iconify/svelte";
 
   export let brandLogo: any;
 </script>
 
 <div
-  class="flex justify-around items-center sticky bottom-0 w-full p-1.5 bg-background-950 text-text-400"
+  class="flex justify-around items-center sticky bottom-0 w-full p-1.5 bg-background-950 text-text-400 text-center leading-none text-sm"
 >
-  <a href="/">PRs</a>
+  <a href="/news">
+    <div>
+      <Icon icon="ion:newspaper" class="w-8 h-8 mx-auto" />
+      <p>News</p>
+    </div>
+  </a>
+  <a href="/">
+    <div>
+      <Icon icon="ion:barbell" class="w-10 h-10 mx-auto -my-1" />
+      <p>PRs</p>
+    </div>
+  </a>
+  <a href="/settings">
+    <div>
+      <Icon icon="ic:round-settings" class="w-8 h-8 mx-auto" />
+      <p>Settings</p>
+    </div>
+  </a>
 </div>
