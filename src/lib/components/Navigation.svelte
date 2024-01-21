9837e2b63bc1480e9ee7c1b8dc09b50a40533416
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-01-21 02:18:37 -0500

    feat: Initial current page highlight

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,31 +1,60 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
-  const dispatch = createEventDispatcher();
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
 
   import Icon from "@iconify/svelte";
 
   export let brandLogo: any;
+
+  let links: any[] = [];
+  let xPosition = 0;
+
+  $: {
+    if (links[0] && links[1] && links[2]) {
+      handleHighlight($page.url.pathname);
+    }
+  }
+
+  onMount(() => window.addEventListener("resize", () => handleHighlight($page.url.pathname)));
+
+  function handleHighlight(page: string) {
+    let currentPage: number;
+    switch (page) {
+      case "/news":
+        currentPage = 0;
+        break;
+      case "/settings":
+        currentPage = 2;
+        break;
+      default:
+        currentPage = 1;
+        break;
+    }
+
+    xPosition = links[currentPage].offsetLeft - (80 - links[currentPage].offsetWidth) / 2;
+  }
 </script>
 
 <div
   class="flex justify-around items-center sticky bottom-0 w-full p-1.5 bg-background-950 text-text-400 text-center leading-none text-sm"
 >
-  <a href="/news">
+  <a href="/news" bind:this={links[0]} class="z-10">
     <div>
       <Icon icon="ion:newspaper" class="w-8 h-8 mx-auto" />
       <p>News</p>
     </div>
   </a>
-  <a href="/">
+  <a href="/" bind:this={links[1]} class="z-10">
     <div>
       <Icon icon="ion:barbell" class="w-10 h-10 mx-auto -my-1" />
       <p>PRs</p>
     </div>
   </a>
-  <a href="/settings">
+  <a href="/settings" bind:this={links[2]} class="z-10">
     <div>
       <Icon icon="ic:round-settings" class="w-8 h-8 mx-auto" />
       <p>Settings</p>
     </div>
   </a>
+  <div class="absolute top-0 w-20 h-full bg-slate-500 rounded-[3rem]" style={"left: " + xPosition + "px"} />
 </div>
