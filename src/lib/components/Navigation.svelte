695c07399cb4e2005bb40bbac86abff520a493d0
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-01-24 23:08:14 -0500

    feat: Backend for news alert system

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -4,18 +4,22 @@
 
   import Icon from "@iconify/svelte";
 
+  import { lastUpdate } from "$lib/assets/meta/news.json";
+  import { preferences } from "$lib/indy";
+
   export let brandLogo: any;
 
   let links: any[] = [];
   let slide = false;
   let xPosition = 0;
+  let newNews = false;
 
   $: {
     if (links[0] && links[1] && links[2]) {
       setTimeout(() => {
         slide = true;
         handleHighlight($page.url.pathname);
-        setTimeout(() => slide = false, 200);
+        setTimeout(() => (slide = false), 200);
       }, 1);
     }
   }
@@ -25,6 +29,16 @@
       handleHighlight($page.url.pathname);
     });
     handleHighlight($page.url.pathname);
+
+    preferences.getItem<number>("lastNewsCheck").then((lastNewsCheck) => {
+      if (lastNewsCheck) {
+        if (lastUpdate > lastNewsCheck) {
+          newNews = true;
+        }
+      } else {
+        preferences.setItem("lastNewsCheck", Date.now());
+      }
+    });
   });
 
   function handleHighlight(page: string) {
@@ -43,12 +57,16 @@
 
     xPosition = links[currentPage].offsetLeft - (96 - links[currentPage].offsetWidth) / 2;
   }
+
+  function setNewsCheck() {
+    preferences.setItem("lastNewsCheck", Date.now());
+  }
 </script>
 
 <div
   class="flex justify-around items-center sticky bottom-0 w-full px-1.5 py-4 bg-background-950 text-text-400 text-center leading-none text-sm overflow-hidden"
 >
-  <a href="/news" bind:this={links[0]} class="z-10">
+  <a href="/news" on:click={setNewsCheck} bind:this={links[0]} class="z-10">
     <div>
       <Icon icon="ion:newspaper" class="w-8 h-8 mx-auto" />
       <p>News</p>
@@ -66,7 +84,11 @@
       <p>Settings</p>
     </div>
   </a>
-  <div class="absolute -top-[9px] w-24 h-24 bg-background-1000 rounded-[3rem]" class:slide={slide} style={"left: " + xPosition + "px"} />
+  <div
+    class="absolute -top-[9px] w-24 h-24 bg-background-1000 rounded-[3rem]"
+    class:slide
+    style={"left: " + xPosition + "px"}
+  />
 </div>
 
 <style>
