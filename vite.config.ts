ac12c79e582ff9077d0788bf1007078b05373377
Cole Kelling <66281330+ThePoultryMan@users.noreply.github.com>
2024-02-17 16:28:38 -0500

    dev: Add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		visualizer({
+			emitFile: true,
+			filename: "stats.html",
+		}),
 	],
 	define: {
 		process: process,
