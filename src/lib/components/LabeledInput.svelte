8ad6234e0877825aaf9e0057993f7455f248d81a
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-04-14 19:29:16 -0400

    refactor: adjust setting styles

--- src/lib/components/LabeledInput.svelte
+++ src/lib/components/LabeledInput.svelte
@@ -5,26 +5,24 @@
     inputId?: string;
     label?: string;
     flipped?: boolean;
-    children?: import('svelte').Snippet;
+    children?: import("svelte").Snippet;
   }
 
-  let {
-    inputId = "",
-    label = "",
-    flipped = false,
-    children
-  }: Props = $props();
+  let { inputId = "", label = "", flipped = false, children }: Props = $props();
 </script>
 
-<div class="inline-flex items-stretch w-fit text-text-400 text-lg" class:flex-row-reverse={flipped}>
+<div
+  class="inline-flex items-stretch w-full text-text-400 text-lg"
+  class:flex-row-reverse={flipped}
+>
   <label
     for={inputId}
-    class="inline-block w-fit h-fit p-1 pr-2 border border-accent-500"
+    class="inline-block h-fit flex-1 p-1 pr-2 border border-accent-500"
     class:rounded-l-lg={!flipped}
     class:rounded-r-lg={flipped}>{label}</label
   >
   <div
-    class="flex items-center *:h-9 *:p-1 *:bg-accent-500 bg-accent-500"
+    class="flex w-fit items-center *:h-9 *:p-1 *:bg-accent-500 bg-accent-500"
     class:rounded-not-flipped={!flipped}
     class:rounded-flipped={flipped}
   >
--- src/routes/more/settings/+page.svelte
+++ src/routes/more/settings/+page.svelte
@@ -20,10 +20,11 @@
   $inspect(preferences.get());
 </script>
 
-<div class="mt-5 ml-8">
-  <h1 class="mb-3 text-xl font-semibold">Settings</h1>
-  <h2 class="mb-1 text-lg">Preferences</h2>
-  <div class="flex">
+<div class="mt-5 mx-8">
+  <h1 class="mb-1.5 text-2xl font-semibold">Settings</h1>
+  <hr class="mb-3" />
+  <h2 class="mb-1 text-xl">Preferences</h2>
+  <div>
     <LabeledInput inputId="barbellWeightDefault" label="Default Barbell Weight">
       {#if barbellWeight}
         <select id="barbellWeightDefault" bind:value={barbellWeight}>
